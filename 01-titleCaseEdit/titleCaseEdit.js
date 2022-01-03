1919337dfb88b4f33f4d5b9d03c1d42755efd617
Lubna Khalid <lkhalid0095@bths.edu>
2022-01-03 17:03:45 -0500

    01-test passed

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,17 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  let count = 0;
+  let str = title.split(" ");
+  let length = str.length;
+  let temp = "";
+  let final = new Array();
+  //change the first letter to capital
+  for(let i=0;i<length; i++){
+    temp = str[i].charAt(0).toUpperCase();
+    temp += str[i].substr(1);
+    final[i]= temp;
+  }
+  title = final.join(" ");
+  return title;
 }
 
 // Do not edit this line;
